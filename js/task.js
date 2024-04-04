ce7a5431e5e3a68f9c6c58cc94db3561cbaa2a11
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-04-04 23:51:03 +0300

    Thursday

--- js/task.js
+++ js/task.js
@@ -1,13 +1,13 @@
-// var button = document.querySelector("button");
-// var box = document.getElementById("changeMe");
+var button = document.querySelector("button");
+var box = document.getElementById("changeMe");
 
-// function changeColor() {
-//   if (box.style.background == "red") {
-//     box.style.background = "blue";
-//   } else {
-//     box.style.background = "red";
-//   }
-// }
+function changeColor() {
+  if (box.style.background == "red") {
+    box.style.background = "blue";
+  } else {
+    box.style.background = "red";
+  }
+}
 
 var box = document.getElementById("box");
 var details = {
