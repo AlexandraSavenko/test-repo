421e85c0a079c9403c78b2fb5810018aa3841977
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-05-29 18:56:17 +0300

    Changes important
    they are important to understand context of this

--- js/myscript.js
+++ js/myscript.js
@@ -157,41 +157,156 @@
 // function add(a, b) {
 //   return a + b;
 // }
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  {
-    title: "The Dreams in the Witch House",
-    author: "Howard Lovecraft",
-    rating: 8.67,
-  },
-];
-const MIN_BOOK_RATING = 8;
-function foo(array) {
-  const newArr = [];
-  for (let book of books) {
-    if (book.rating >= MIN_BOOK_RATING) {
-      newArr.push(book.author);
-    }
-  }
-  console.log(newArr);
-}
-console.log(foo(books));
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// function foo(array) {
+//   const newArr = [];
+//   for (let book of books) {
+//     if (book.rating >= MIN_BOOK_RATING) {
+//       newArr.push(book.author);
+//     }
+//   }
+//   console.log(newArr);
+// }
+// console.log(foo(books));
 // const names = books
 //   .filter((book) => book.rating >= MIN_BOOK_RATING)
 //   .map((book) => book.author)
 //   .toSorted((a, b) => a.localeCompare(b));
+
+// function hello() {
+//   console.log(this.name);
+// }
+// hello();
+// const objA = {
+//   name: "Alice",
+// };
+
+// hello.call(objA);
+
+// const objA = {
+//   name: "Alice",
+//   hello: () => {
+//     console.log(this.name); // ''
+//   },
+// };
+
+// const objB = {
+//   name: "Kate",
+//   hello() {
+//     objA.hello();
+//   },
+// };
+
+// objB.hello();
+
+// const hello = function () {
+//   console.log(this.name);
+// };
+
+// const objA = {
+//   name: "Alice",
+//   myName: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// objA.myName();
+//!
+// const objA = {
+//   name: "Alice",
+//   myName: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// const objB = {
+//   name: "Kate",
+//   hello: objA.myName,
+// };
+
+// objB.hello();
+//!
+// const objA = {
+//   name: "Alice",
+//   myName: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// const objB = {
+//   name: "Kate",
+//   hello: objA.myName,
+// };
+
+// const fn = objB.hello;
+// fn();
+// console.log(fn);
+//When I add () to const fn = objB.hello, it works and shoes "Kate", but fn() gives Error because fn is not a function
+//==========================================================
+// const hello = () => {
+//   console.log(this.name);
+// };
+
+// const objA = {
+//   name: "Alice",
+//   myName: hello,
+// };
+
+// objA.myName();
+
+// const flower = {
+//   blooming: "rose",
+//   sun: function () {
+//     console.log(this.blooming);
+//   },
+// };
+// const tree = {
+//   leaves: "green",
+//   branch: flower.sun.bind(flower),
+//   total() {
+//     console.log(`This tree ${this.leaves} is ${flower.blooming}`);
+//   },
+// };
+// tree.branch();
+// tree.total();
+//!!!!!!!!
+// const objA = {
+//   name: "Alice",
+//   objB: {
+//     name: "Beth",
+//     objC: {
+//       name: "Cassy",
+//       wrapper() {
+//         const hello = () => {
+//           console.log(this);
+//         };
+//         hello();
+//       },
+//     },
+//   },
+// };
+// objA.objB.objC.wrapper();
+//!!!!!!
